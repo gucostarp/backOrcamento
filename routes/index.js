fc70fcc97b4da2cb3f47da70821c67f229fadd3b
Gustavo Costa <gucostarp@gmail.com>
2021-03-12 16:13:25 -0300

    Preparação para o DB

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,12 @@ const clientes = [
   {
     id: 1,
     nome: 'UNESP Bauru',
+    contato: 'Severino',
+    departamento: 'Compras',
     cpfcnpj: '10.435.371/0001-50',
+    telefone: 16363636363, 
     email: 'unesp@unesp.br',
-  },
-  {
-    id: 2,
-    nome: 'Universidade de São Paulo',
-    cpfcnpj: '20.225.314/0001-20',
-    email: 'usp@usp.br',
-  },
-  {
-    id: 3,
-    nome: 'UNAERP Universidade de Ribeirão Preto',
-    cpfcnpj: '15.684.124/0001-12',
-    email: 'unaerp@unaerp.br',
-  },
-  
+  },  
 ];
 
 const produtos = [
@@ -41,6 +31,7 @@ const produtos = [
     garantia: '4 meses',
   },
 ];
+
 const orcamentos = [
   {
     id: 1,
@@ -58,20 +49,42 @@ const orcamentos = [
   },
 ];
 
+// Obtém uma lista de todos os clientes
 router.get('/clientes', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
   res.send(clientes)
 });
 
+// Obtém um único cliente pelo seu id
+router.get('/clientes/:id', function(req, res, next) {
+
+  const cliente = clientes.filter((cliente)=>{
+    return cliente.id == req.params.id;
+  });
+
+  res.send(cliente[0])
+});
+
+// Insere um novo cliente na lista de clientes
+router.post('/clientes', function(req, res, next) {
+  clientes.push(req.body);
+  res.send("Inserido com sucesso");
+});
+
+// Altera um cliente na lista de clientes
+router.put('/clientes/:id', function(req, res, next) {
+  // clientes.push(req.body);
+  res.send("Alterado com sucesso");
+});
+
+
+
 router.get('/produtos', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
   res.send(produtos)
 });
 
 module.exports = router;
 
 router.get('/orcamentos', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
   res.send(orcamentos)
 });
 
